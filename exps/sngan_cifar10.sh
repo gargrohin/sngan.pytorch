c42a414810d3e41301d5aecbbeb3bb8fb0576dde
gargrohin <gargrohin0@gmail.com>
2020-09-15 14:30:18 -0700

    updates

--- exps/sngan_cifar10.sh
+++ exps/sngan_cifar10.sh
@@ -12,9 +12,9 @@ python train.py \
 --gf_dim 256 \
 --df_dim 128 \
 --g_spectral_norm False \
---d_spectral_norm False \
+--d_spectral_norm True \
 --g_lr 0.0002 \
---d_lr 0.0002 \
+--d_lr 0.0001 \
 --beta1 0.0 \
 --beta2 0.9 \
 --init_type xavier_uniform \
