ee3db6e706a00b506df5f41f2f24a3ad7b0214c2
gargrohin <gargrohin0@gmail.com>
2020-09-21 08:04:53 -0700

    wgangp updates

--- train.py
+++ train.py
@@ -146,7 +146,7 @@ def main():
         'valid_global_steps': start_epoch // args.val_freq,
     }
 
-    experiment = comet_ml.Experiment(project_name="wgangp-resnet")
+    experiment = comet_ml.Experiment(project_name="wgangp-sngan")
     exp_parameters = {
         "data": "cifar10_32x32",
         "model": "wgangp-cifar10",
